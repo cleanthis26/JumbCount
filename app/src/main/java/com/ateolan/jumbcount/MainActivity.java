47e8d184b8cb0c4bc616f72d0d164333bcb23513
Cleanthis <cleanthis26@hotmail.com>
2016-04-23 02:32:58 +0300

    Minus double click

--- app/src/main/java/com/ateolan/jumbcount/MainActivity.java
+++ app/src/main/java/com/ateolan/jumbcount/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int DOUBLE_PRESS_INTERVAL = 1000;
+
     // Variables
     int jumbCounter;
+    long lastPressTime;
 
     // Components
     TextView counterTV;
@@ -34,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void minusButton_onClick(View v) {
+        long oldPressTime = lastPressTime;
+        lastPressTime = System.currentTimeMillis();
+        if (lastPressTime - oldPressTime > DOUBLE_PRESS_INTERVAL) {
+            Toast.makeText(getApplicationContext(), "Press twice to remove", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        lastPressTime = 0; // Reset
+
         if (jumbCounter <= 0) {
             jumbCounter = 0;
         } else {
